7e8b4924cba02f4efe52894a6ca2471d2ec2143e
Shivam Goyal <glyshivam@gmail.com>
2024-06-30 17:31:18 +0530

    feat: added common utility script
    The common utility script will contain the methods to automate day-to-day tasks. Added methods to trigger notifications on mac-os using apple script.

---
+++ common.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+
+# Triggers a default notification (usually used to get a notification after a long
+# running process is finished) for example: ffmpeg -i input out && nd
+nd() {
+    currentTime=`date +"%I:%M %p"`
+    notify "Process finished at $currentTime!"
+}
+
+# Triggers a notification using apple script
+notify() {
+    content=$1
+    title="${2:-Terminal}"
+    subtitle=$3
+
+    osascript -e "display notification \"$content\" with title \"$title\" subtitle \"$subtitle\""
+}
+
---
+++ index.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+source ~/.shell-utils/common.sh
+source ~/.shell-utils/remote-desktop.sh
